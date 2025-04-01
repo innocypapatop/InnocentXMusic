311695c52b48dc9ce2f10458f19671aaac1d20a5
innocypapatop <moonwalahehe@gmail.com>
2025-04-02 00:30:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/INNOCENTBOY2926/InnocentXMusic",
+    "https://github.com/innocypapatop/InnocentXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
